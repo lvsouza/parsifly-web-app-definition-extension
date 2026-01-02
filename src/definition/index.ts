db41100105aec7ea8abb2e9a98d103dfc17eef4a
Lucas Souza <ls18017@gmail.com>
2026-01-02 13:03:02 -0300

    feat: add structure table

--- src/definition/index.ts
+++ src/definition/index.ts
@@ -110,43 +110,62 @@ export const createDefinition = (application: TApplication) => {
         description: 'Create the project folders table',
         upQuery: () => sql`
           -- Create function
-          CREATE OR REPLACE FUNCTION prevent_folder_cycles()
+          CREATE OR REPLACE FUNCTION prevent_cycles_generic()
           RETURNS trigger AS $$
+          DECLARE
+            table_name text := TG_ARGV[0];
+            id_column text := TG_ARGV[1];
+            parent_column text := TG_ARGV[2];
+
+            sql text;
+            has_cycle boolean;
           BEGIN
-            IF NEW."parentFolderId" IS NULL THEN
+            -- Se o parent for NULL, não há risco
+            EXECUTE format(
+              'SELECT ($1).%I IS NULL',
+              parent_column
+            )
+            USING NEW
+            INTO has_cycle;
+
+            IF has_cycle THEN
               RETURN NEW;
             END IF;
 
-            IF EXISTS (
+            sql := format($sql$
               WITH RECURSIVE ancestors AS (
-                -- Anchor
-                SELECT id, "parentFolderId"
-                FROM folder
-                WHERE id = NEW."parentFolderId"
+                SELECT %1$I AS id, %2$I AS parent_id
+                FROM %3$I
+                WHERE %1$I = ($1).%2$I
 
                 UNION ALL
 
-                -- Recursion
-                SELECT f.id, f."parentFolderId"
-                FROM folder f
-                JOIN ancestors a ON f.id = a."parentFolderId"
-                -- Repare: não precisamos mais do WHERE NOT IN manual aqui
+                SELECT t.%1$I, t.%2$I
+                FROM %3$I t
+                JOIN ancestors a ON t.%1$I = a.parent_id
               )
-              -- A Mágica do Postgres 14+:
               CYCLE id SET is_cycle USING path
-              
-              SELECT 1
-              FROM ancestors
-              WHERE id = NEW.id
-              -- Opcional: Se quiser garantir que não considera ciclos pre-existentes como o próprio erro:
-              AND is_cycle IS FALSE 
-            ) THEN
+              SELECT EXISTS (
+                SELECT 1
+                FROM ancestors
+                WHERE id = ($1).%1$I
+                  AND is_cycle IS FALSE
+              )
+            $sql$,
+              id_column,
+              parent_column,
+              table_name
+            );
+
+            EXECUTE sql USING NEW INTO has_cycle;
+
+            IF has_cycle THEN
               RAISE EXCEPTION
                 USING
                   ERRCODE = 'P1001',
-                  MESSAGE = 'Invalid folder hierarchy',
-                  DETAIL = 'A folder cannot be moved into itself or one of its descendants',
-                  HINT = 'Choose a different parent folder';
+                  MESSAGE = format('Invalid hierarchy in %s', table_name),
+                  DETAIL = 'An entity cannot be moved into itself or one of its descendants',
+                  HINT = 'Choose a different parent';
             END IF;
 
             RETURN NEW;
@@ -157,15 +176,19 @@ export const createDefinition = (application: TApplication) => {
       {
         order: 4,
         id: '004__create-folder-trigger-to-call-function-to-prevent-self-reference',
-        description: 'Create the project folders table',
+        description: 'Create trigger for prevent self reference',
         upQuery: () => sql`
-          -- Create trigger in the table to call the function
+          -- Create trigger
           CREATE TRIGGER folder_no_cycles
           BEFORE INSERT OR UPDATE OF "parentFolderId"
           ON folder
           FOR EACH ROW
           WHEN (NEW."parentFolderId" IS NOT NULL)
-          EXECUTE FUNCTION prevent_folder_cycles();
+          EXECUTE FUNCTION prevent_cycles_generic(
+            'folder',
+            'id',
+            'parentFolderId'
+          );
         `.compile(databaseHelper),
       },
       {
@@ -262,7 +285,139 @@ export const createDefinition = (application: TApplication) => {
       },
       {
         order: 9,
-        id: '009_create-project-item',
+        id: '009__create-web-app-data-types',
+        description: 'Create web app data types',
+        upQuery: () => databaseHelper.schema
+          .createType('enum_web_app_data_type')
+          .asEnum([
+            'structure',
+
+            'string',
+            'number',
+            'boolean',
+            'null',
+            'object',
+            'binary',
+
+            'array_structure',
+
+            'array_string',
+            'array_number',
+            'array_boolean',
+            'array_null',
+            'array_object',
+            'array_binary',
+          ])
+          .compile(),
+      },
+      {
+        order: 10,
+        id: '0010__create-structure-attribute-table',
+        description: 'Create the project structure attributes table',
+        upQuery: () => databaseHelper.schema
+          .createTable('structureAttribute')
+          .addColumn('id', 'uuid', col => col.primaryKey().notNull().defaultTo(sql`gen_random_uuid()`))
+          .addColumn('name', 'varchar', col => col.notNull().unique())
+          .addColumn('type', 'varchar', col => col.notNull().check(sql`type in ('structure-attribute')`).defaultTo('structure-attribute'))
+          .addColumn('description', 'varchar')
+          .addColumn('createdAt', 'timestamptz', col => col.notNull().defaultTo(sql`now()`))
+
+          .addColumn('projectOwnerId', 'uuid', col => col.notNull().references('project.id').onDelete('cascade'))
+
+
+          // CHECKs: Garantem que o dataType não pode ser alterado para um type diferente do tipo do defaultValue, deixe o defaultValue null então altere o dataType para oq ue quiser
+          .addColumn('dataType', sql`enum_web_app_data_type`, col => col.notNull().check(sql`"defaultValue" IS NULL OR ("dataType"::text IN ('string','number','boolean') AND jsonb_typeof("defaultValue") = "dataType"::text)`))
+          // CHECKs: Garantem que nunca possa ser salva um valor diferente de null, string, number ou boolean
+          .addColumn('defaultValue', 'jsonb', col => col.check(sql`"defaultValue" IS NULL OR ("dataType"::text IN ('string','number','boolean') AND jsonb_typeof("defaultValue") = "dataType"::text)`))
+          .addColumn('required', 'boolean', col => col.notNull().defaultTo(false))
+
+
+          .addColumn('parentStructureId', 'uuid', col => col.references('structure.id').onDelete('cascade'))
+          .addColumn('parentStructureAttributeId', 'uuid', col => col.references('structureAttribute.id').onDelete('cascade'))
+          .addCheckConstraint(
+            'structureAttribute__structure_or_structureAttribute_not_null',
+            sql`(("parentStructureId" IS NOT NULL AND "parentStructureAttributeId" IS NULL) OR ("parentStructureId" IS NULL AND "parentStructureAttributeId" IS NOT NULL))`
+          )
+          .addUniqueConstraint(
+            'structureAttribute__unique_name_for__name_of_parentStructureId_parentStructureAttributeId',
+            ['name', 'parentStructureId', 'parentStructureAttributeId'],
+            builder => builder.nullsNotDistinct()
+          )
+          .compile(),
+      },
+      {
+        order: 11,
+        id: '0011__create-structureAttribute-trigger-to-call-function-to-prevent-self-reference',
+        description: 'Create trigger for prevent self reference',
+        upQuery: () => sql`
+          -- Create trigger
+          CREATE TRIGGER structure_attribute_no_cycles
+          BEFORE INSERT OR UPDATE OF "parentStructureAttributeId"
+          ON "structureAttribute"
+          FOR EACH ROW
+          WHEN (NEW."parentStructureAttributeId" IS NOT NULL)
+          EXECUTE FUNCTION prevent_cycles_generic(
+            'structureAttribute',
+            'id',
+            'parentStructureAttributeId'
+          );
+        `.compile(databaseHelper),
+      },
+      {
+        order: 12,
+        id: '0012__create-structureAttribute-function-to-prevent-create-link-to-a-parent-not-in-object-or-array_object',
+        description: 'Create function to prevent parent not in (object ou array_object)',
+        upQuery: () => sql`
+          -- Create function
+          CREATE OR REPLACE FUNCTION validate_parent_structure_attribute()
+          RETURNS trigger AS $$
+          DECLARE
+            parent_type text;
+          BEGIN
+            -- Se não tem parent, não valida nada
+            IF NEW."parentStructureAttributeId" IS NULL THEN
+              RETURN NEW;
+            END IF;
+
+            SELECT "dataType"
+            INTO parent_type
+            FROM "structureAttribute"
+            WHERE "id" = NEW."parentStructureAttributeId";
+
+            IF parent_type NOT IN ('object', 'array_object') THEN
+              RAISE EXCEPTION
+                USING
+                  MESSAGE = 'Invalid parent structure attribute',
+                  DETAIL = 'Parent attribute must have dataType object or array_object',
+                  HINT = 'Choose an attribute with a compatible dataType';
+            END IF;
+
+            RETURN NEW;
+          END;
+          $$ LANGUAGE plpgsql;
+        `.compile(databaseHelper),
+      },
+      {
+        order: 13,
+        id: '0013__create-structureAttribute-trigger-to-call-function-to-prevent-create-link-to-a-parent-not-in-object-or-array_object',
+        description: 'Create trigger for prevent parent not in object or array_object',
+        upQuery: () => sql`
+          -- Create trigger
+          CREATE TRIGGER structure_attribute_parent_check
+          BEFORE INSERT OR UPDATE OF "parentStructureAttributeId"
+          ON "structureAttribute"
+          FOR EACH ROW
+          EXECUTE FUNCTION validate_parent_structure_attribute();
+        `.compile(databaseHelper),
+      },
+
+
+
+
+
+      {
+        order: 100,
+        id: '00100_create-project-item',
         description: 'Create the project item',
         upQuery: () => databaseHelper
           .insertInto('project')
