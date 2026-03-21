9839ab96ab1f5d466ddaf1a7879e02e4595ad42e
Lucas Souza <ls18017@gmail.com>
2026-03-21 10:13:46 -0300

    feat: add completion for external actions

---
+++ src/completions/externalActions.ts
@@ -0,0 +1,62 @@
+import { CompletionsDescriptor, CompletionViewItem, TExtensionContext } from 'parsifly-extension-base';
+
+import { createDatabaseHelper } from '../definition/DatabaseHelper';
+import { externalAction } from '../definition/schema';
+
+
+export const createExternalActionCompletionsDescriptor = (extensionContext: TExtensionContext) => {
+  const databaseHelper = createDatabaseHelper(extensionContext);
+
+  return new CompletionsDescriptor({
+    key: 'externalAction',
+    onGetCompletions: async (intent) => {
+      if (intent.visibility?.type === 'projectListener') {
+        const externalActions = await databaseHelper
+          .select({
+            id: externalAction.id,
+            name: externalAction.name,
+            type: externalAction.type,
+            description: externalAction.description
+          })
+          .from(externalAction)
+
+        if (intent.kind === 'callable') return [
+          ...externalActions.map(externalAction => (
+            new CompletionViewItem({
+              key: externalAction.id,
+              initialValue: {
+                label: externalAction.name,
+                icon: { path: 'external-action.svg' },
+                value: { type: 'externalAction', referenceId: externalAction.id },
+              },
+            })
+          )),
+        ];
+      } else if (intent.visibility?.type === 'componentListener') {
+        const externalActions = await databaseHelper
+          .select({
+            id: externalAction.id,
+            type: externalAction.type,
+            name: externalAction.name,
+            description: externalAction.description
+          })
+          .from(externalAction)
+
+        if (intent.kind === 'callable') return [
+          ...externalActions.map(externalAction => (
+            new CompletionViewItem({
+              key: externalAction.id,
+              initialValue: {
+                label: externalAction.name,
+                icon: { path: 'external-action.svg' },
+                value: { type: 'externalAction', referenceId: externalAction.id },
+              },
+            })
+          )),
+        ];
+      }
+
+      return [];
+    }
+  })
+}
--- src/completions/index.ts
+++ src/completions/index.ts
@@ -3,6 +3,7 @@ import { TExtensionContext } from 'parsifly-extension-base';
 import { createComponentActionCompletionsDescriptor } from './componentActions';
 import { createGlobalDataTypeCompletionsDescriptor } from './globalDataTypes';
 import { createComponentEventCompletionsDescriptor } from './componentEvents';
+import { createExternalActionCompletionsDescriptor } from './externalActions';
 import { createProjectActionCompletionsDescriptor } from './projectActions';
 import { createExternalEventCompletionsDescriptor } from './externalEvents';
 import { createProjectEventCompletionsDescriptor } from './projectEvents';
@@ -12,6 +13,7 @@ export const registerCompletions = (extensionContext: TExtensionContext) => {
   const componentActionCompletionsDescriptor = createComponentActionCompletionsDescriptor(extensionContext);
   const globalDataTypeCompletionsDescriptor = createGlobalDataTypeCompletionsDescriptor(extensionContext);
   const componentEventCompletionsDescriptor = createComponentEventCompletionsDescriptor(extensionContext);
+  const externalActionCompletionsDescriptor = createExternalActionCompletionsDescriptor(extensionContext);
   const projectActionCompletionsDescriptor = createProjectActionCompletionsDescriptor(extensionContext);
   const externalEventCompletionsDescriptor = createExternalEventCompletionsDescriptor(extensionContext);
   const projectEventCompletionsDescriptor = createProjectEventCompletionsDescriptor(extensionContext);
@@ -19,6 +21,7 @@ export const registerCompletions = (extensionContext: TExtensionContext) => {
   extensionContext.completions.register(componentActionCompletionsDescriptor);
   extensionContext.completions.register(globalDataTypeCompletionsDescriptor);
   extensionContext.completions.register(componentEventCompletionsDescriptor);
+  extensionContext.completions.register(externalActionCompletionsDescriptor);
   extensionContext.completions.register(projectActionCompletionsDescriptor);
   extensionContext.completions.register(externalEventCompletionsDescriptor);
   extensionContext.completions.register(projectEventCompletionsDescriptor);
@@ -27,8 +30,9 @@ export const registerCompletions = (extensionContext: TExtensionContext) => {
     extensionContext.completions.unregister(projectEventCompletionsDescriptor);
     extensionContext.completions.unregister(externalEventCompletionsDescriptor);
     extensionContext.completions.unregister(projectActionCompletionsDescriptor);
+    extensionContext.completions.unregister(externalActionCompletionsDescriptor);
     extensionContext.completions.unregister(componentEventCompletionsDescriptor);
     extensionContext.completions.unregister(globalDataTypeCompletionsDescriptor);
     extensionContext.completions.unregister(componentActionCompletionsDescriptor);
-  }
-}
+  };
+};
